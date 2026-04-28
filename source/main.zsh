ecb6f3797a53e5a8deb0f26202eace9877ccc38c
JV <jaiveer.chadda@gmail.com>
2026-04-28 10:29:26 +0100

    Completely reworked the `see` function from the ground up. There's honestly not even much point going through everything new, but most importantly, I fixed the issue of trailing / leading newlines/spaces in the input, created a bunch of new options/flags, and added quite a few new colours to everything. Very happy with it all I can't lie lol

--- source/main.zsh
+++ source/main.zsh
@@ -2,6 +2,12 @@
 
 # — TODO ———————————————————————————————————————————————————————————————————— #
 
+# BUGS
+# ‾‾‾‾
+# - For some reason the input I'm getting seems to have all the leading spaces
+#    stripped out.
+#   - I'll have to check out what's causing the issue
+
 # Features
 # ‾‾‾‾‾‾‾‾
 # - implement a different highlight for different kinds of characters:
@@ -42,7 +48,7 @@
 
 # To Finish
 # ‾‾‾‾‾‾‾‾‾
-# - implement the usage of the `-c` flag
+# - implement the usage of the `-l` flag
 # - create a proper verbose `-v` mode
 # - make an actual usage/`-h` message
 
@@ -58,7 +64,6 @@
 #
 # - add a flag to change how the space char ␣ is displayed
 #   - i.e. which one is used, if any at all
-
 
 # ——————————————————————————————————————————————————————————————————————————— #
 
@@ -177,45 +182,38 @@ see() {
 
   # — Constants ———————————————————————————————————————————————————————————— #
 
-  local -r _NL=$'\n'
-  local -r _SP=' '
-  local -r _NL_0x='a'
-  local -r _SP_0x='20'
+  local _reset=$'\e[0m'
+  local -r _unicode_colour="$_reset"$'\e[1;38;5;231;48;5;088m'
+  local -r _c_style_colour="$_reset"$'\e[1;38;5;033;48;5;236m'
+  local -r   _caret_colour="$_reset"$'\e[1;38;5;226;48;5;018m'
 
-  local -r _reset=$'\e[0m'
-  local -r       _c_blue="$_reset"$'\e[38;5;033;48;5;236m'
-  local -r  _unicode_red="$_reset"$'\e[38;5;231;48;5;088m'
-  local -r _caret_yellow="$_reset"$'\e[38;5;226;48;5;018m'
+  local -r _SP_char='·'  # '␣' / '·' / '␠' / ' '
+  local -r _SP_colour=$'\e[49;1;38;5;033m'
+  # local -r _NL_colour=$'\e[39;1;48;5;026m'
+  local -r _NL_colour=$'\e[49;1;33m'
 
-  local -r   _space_blue="$_reset"$'\e[38;5;033m'
-
-  local -r _space_char='␣'  # '␣' / '␠' / ' '
-  local -r _space_repr="$_space_blue␣$_reset"
-
+  local -r _line="${(r:$COLUMNS::─:)}"
 
   local -rA _none_esc_chars=(
   )
-
   local -rA _c_esc_chars=(
-    [esc_col]="$_c_blue"
+    [esc_col]="$_c_style_colour"
     [$'\u00']='\\0' [$'\u07']='\\a' [$'\u08']='\\b' [$'\u09']='\\t'
     [$'\u0A']='\\n' [$'\u0B']='\\v' [$'\u0C']='\\f' [$'\u0D']='\\r'
     [$'\u1B']='\\e'
   )
-
   local -rA _unicode_esc_chars=(
-    [esc_col]="$_unicode_red"
+    [esc_col]="$_unicode_colour"
     [$'\u00']='␀'  [$'\u01']='␁'  [$'\u02']='␂'  [$'\u03']='␃'  [$'\u04']='␄'
     [$'\u05']='␅'  [$'\u06']='␆'  [$'\u07']='␇'  [$'\u08']='␈'  [$'\u09']='␉'
-    [$'\u0A']='␊'  [$'\u0B']='␋'  [$'\u0C']='␌'  [$'\u0D']='␍'  [$'\u0E']='␎'
+    [$'\u0A']='␤'  [$'\u0B']='␋'  [$'\u0C']='␌'  [$'\u0D']='␍'  [$'\u0E']='␎'
     [$'\u0F']='␏'  [$'\u10']='␐'  [$'\u11']='␑'  [$'\u12']='␒'  [$'\u13']='␓'
     [$'\u14']='␔'  [$'\u15']='␕'  [$'\u16']='␖'  [$'\u17']='␗'  [$'\u18']='␘'
     [$'\u19']='␙'  [$'\u1A']='␚'  [$'\u1B']='␛'  [$'\u1C']='␜'  [$'\u1D']='␝'
     [$'\u1E']='␞'  [$'\u1F']='␟'  [$'\u7F']='␡'
   )
-
   local -rA _caret_esc_chars=(
-    [esc_col]="$_caret_yellow"
+    [esc_col]="$_caret_colour"
     [$'\u00']='^@' [$'\u01']='^A' [$'\u02']='^B' [$'\u03']='^C' [$'\u04']='^D'
     [$'\u05']='^E' [$'\u06']='^F' [$'\u07']='^G' [$'\u08']='^H' [$'\u09']='^I'
     [$'\u0A']='^J' [$'\u0B']='^K' [$'\u0C']='^L' [$'\u0D']='^M' [$'\u0E']='^N'
@@ -225,51 +223,70 @@ see() {
     [$'\u1E']='^^' [$'\u1F']='^_' [$'\u7F']='^?'
   )
 
-  # — User Input ——————————————————————————————————————————————————————————— #
+  # — Take User Input —————————————————————————————————————————————————————— #
 
   # Note: a 'u_' prefix indicates a user-inputted value
-  local -i 10 u_debug=0     # [bool] debug mode (implies verbose)
-  local -i 10 u_verbose=0   # [bool] verbose mode
-  local -i 10 u_text_mode=0 # [bool] show just text instead of columns
+  local u_file=             # not implemented yet
+  local u_mode='text'       # only kinda implemented
+
+  local u_do_colours='auto' # when to show the colours
+  local u_colours=          # not implemented yet
+  local u_esc_chars=''      # default is 'unicode', but that's handled below
+
   local -i 10 u_width=32    # width for column mode (≈ xxd -c)
   local -i 10 u_zero_pad=2  # how many 0s to add before a hex code
-  local u_esc_chars=''      # which set of esc chars to use. the options are:
-  #                         #   unicode    c    caret    cdash    none
 
-  while { getopts ':DdvtCw:0:e:' opt; } {
+  local -i 2 u_debug=0      # [bool] debug mode (implies verbose)
+  local -i 2 u_verbose=0    # [bool] verbose mode
 
-    if (( u_debug )) {  
-      echo "${(r:40::─:)}"
-      echo "opt  ==  '$opt'"
-      echo "arg  ==  '$OPTARG'"
-    }
-  
+  # the leading hyphen here turns on some debug info,
+  #  which I capture and use below
+  local opt OPTARG OPTIND
+  while { getopts ':f:m:tlc:C:e:w:0:vDdh' opt; } {
+    #
+    if (( u_debug )) { echo "${(r:40::─:)}\nopt == '$opt'\narg == '$OPTARG'"; }
+    #
     case "$opt" {
-    [Dd]) u_debug=1 u_verbose=1 ;;
-      v ) u_verbose=1           ;;
-      t ) u_text_mode=1         ;;
-      C ) u_text_mode=0         ;;
-      w ) u_width="$OPTARG"     ;;
-      0 ) u_zero_pad="$OPTARG"  ;;
+      #### File ####
+      f ) u_file="$OPTARG"      ;; # NOT IMPLEMENTED
+      #
+      #### Modes ####
+      m ) u_mode="$OPTARG"      ;; # not fully implemented
+      t ) u_mode='text'         ;;
+      l ) u_mode='list'         ;;
+      #
+      #### Graphics ####
+      c ) u_do_colours="$OPTARG";;
+      C ) u_colours="$OPTARG"   ;; # NOT IMPLEMENTED
       e ) u_esc_chars="$OPTARG" ;;
-      h ) usage; return 0       ;;
+      #
+      #### Hex Display ####
+      w ) u_width="$OPTARG"     ;; # no effect yet
+      0 ) u_zero_pad="$OPTARG"  ;;
+      #
+      #### Internal/Debug ####
+      v ) u_verbose=1           ;; # not fully implemented
+     D|d) u_debug=1 u_verbose=1 ;;
+      #
+      #### Usage ####
+      h ) see::usage; return 0  ;;
       * )
-        echo -n $'\e[31m'
-        if [[ "$opt" == '?' ]] { echo "$0: bad option: -$OPTARG" >&2; } \
-        else { echo "$0: -$OPTARG requires an argument" >&2; }  # $opt = ':'
-        echo -n $'\e[0m'
-        usage; return 1
-        ;;
+        echo -n "$0: bad option: -$OPTARG"                  >&2 # if opt == '?'
+        if [[ "$opt" == ':' ]] echo -n ' needs an argument' >&2 # if opt == ':'
+        if (( ! u_debug )) { echo $'\n' >&2; see::usage; return 1; }
+        echo $'\n\e[1;31m——————— Option Issue ———————\e[0m'
+      ;;
     }
-
+    #
     if (( u_debug )) {
-      echo -n "\e[32m-$opt"
+      echo -n $'\e[32m-'"-$opt"
       if [[ -n "$OPTARG" ]] echo -n " ${(qq)OPTARG}"
-      echo ' is a valid input\e[0m'
+      echo $' is a valid input\e[0m'
     }
   }
-
   shift $(( OPTIND - 1 ))
+
+  # — Process User Input ———————————————————————————————————————————————————— #
 
   if (( u_debug )) {
     u_verbose=1
@@ -277,192 +294,192 @@ see() {
     #  - the reasoning behind this is that if $PS4's been changed by the user,
     #    then they probably like it that way.
     #  - but if it hasn't, then we're free to use whichever version we like
-    if [[ "$PS4" == '+%N:%i> ' ]] local PS4="$_CUSTOM_PS4"
+    if [[ "$PS4" == '+%N:%i> ' || "$PS4" == '++' ]] local PS4="$_CUSTOM_PS4"
     set -x
   }
 
+  # —— Set Colours & Special Chars (SP/NL) ————— #
+
+  local -i 2 do_colours=0
+  case "$u_do_colours" {
+    always ) do_colours=1 ;;
+    never  ) do_colours=0 ;;
+    # if stdout `1` is writing to a tty `-t`...
+    # i.e. if the output isn't being being piped somewhere
+    # then turn colours on
+    * ) if [[ -t 1 ]] do_colours=1 ;;
+  }
+
+  # —— Create Charset —————————————————————————— #
+
   # recreate the esc charset variable name from input
-  _charset_name="_${(L)u_esc_chars:-unicode}_esc_chars"
+  local _charset_name="_${(L)u_esc_chars:-unicode}_esc_chars"
   # then pass that input by name (P) into the
   #  assoc array that's gonna be used for displaying chars
-  local -rA esc_chars=( "${(@Pkv)_charset_name}" )
-  local -r esc_col="${esc_chars[esc_col]}"
+  local -A esc_chars=( "${(@Pkv)_charset_name}" )
+
+  # ———————————————————————————————————————————— #
+
+  local -r _NL=$'\n' _SP=' '
+  local esc_col= reset=
+
+  esc_chars[$_SP]="$_SP_char"
+
+  if (( do_colours )) {
+    esc_col="${esc_chars[esc_col]}"; reset="$_reset"
+    # You have to pass the space and newline in as variables, otherwise
+    #  zsh can't process the keys
+    esc_chars[$_SP]="$_SP_colour$_SP_char$_reset"
+    esc_chars[$_NL]="$_NL_colour${esc_chars[$_NL]}$_reset"
+  }
+
+  # Text mode needs an newline for legibility
+  if [[ "$u_mode" == 'text' ]] esc_chars[$_NL]+="$_NL"
+
+  # ————————————————————————————————————————————————————————————————————————— #
+  # — Reading from STDIN ———————————————————————————————————————————————————— #
+
   # read input from stdin, and append a newline to each line
   # note: the `|| [[ -n ...` section allows the last line to be read
   #  if the input doesn't end with a newline
-  # gonna change this later TBAT work with passing a filename/variable in
-  local input line
-  while read -r line || [[ -n "$line" ]] input+="$line$_NL" </dev/stdin
+  #y)TODO: change this later so it works when passing a filename in
 
+  local -r input="${$( cat; echo '.' )%.}"
+
+  # ———————————————————————————————————————————————————————————————————————— #
   # — Pre-Processing ——————————————————————————————————————————————————————— #
 
-  # remove the trailing newline
   # I did this originally to remove the traling newline
   #  added by the `input+=...` line, but idk if/when it's actually needed
-  # TODO: check if/when it's needed
-  input="${input/%$'\n'}"
+  #y)TODO: check if/when it's needed
 
+  # —— Split Input at Codepoints —————————————— #
   # split input at every !!codepoint!!
   #  - i.e. it recognises multi-byte characters
-  # also, replace all spaces with the space repr
-  local -ra chars=(
-    "${(@)${(s::)input}//$_SP/$_space_repr}"
-  )
-  # local k v
-  # for k v in "${(@kv)esc_chars}"; {
-  #   chars=( "${(@)chars//$k/$esc_col$v$_reset}" )
-  # }
+  local -ra chars=( "${(@s::)input}" )
 
+  # —— Convert Chars to Hex ———————————————————— #
   # - take every char and prepend it with a quote: `'`
   # - then use printf to convert each char to hex,
   #   - adding a newline between each hex value
-  # - then split the result by newlines (f), and assign it to an arr
-  local -a hexes=(
-    "${(@f)"$( printf '%x\n' \'${^chars} )"}"
-  )
-  
+  # - then split the result by newlines (f), and assign it to an array (@)
+  local -a hexes=( "${(@f)"$( printf '%x\n' \'${^chars} )"}" )
   # zip $chars and $hexes together
   local -ra result=( "${(@)chars:^hexes}" )
 
+  # ———————————————————————————————————————————————————————————————————————— #
   # — Outputting Results ——————————————————————————————————————————————————— #
 
   local char hex
+  # Even though this looks like associative array syntax, it's not.
+  #  I'm iterating through the zipped chars and hexes arrays, so zsh splits
+  #  them for me, hence the separate char and hex variables
   for char hex in "${(@)result}"; {
-    # if we're in text mode, and char is a newline (0x0a),
-    #  print a newline (`echo`) for legibility
-    if (( "$u_text_mode" )) && [[ "${(L)hex}" == "$_NL_0x" ]] echo
 
+    # —— Replace Chars w Reprs ————————————————— #
     # replace all chars with their special representations, if applicable
     if [[ "${esc_chars[(Ie)$char]}" ]] \
-      char="${esc_chars[esc_col]}${esc_chars[$char]}$_reset"
+      char="$esc_col${esc_chars[$char]}$reset"
 
+    # —— Print Char ———————————————————————————— #
+    # always print the char itself
+    # note: this syntax seemed like the only thing that works with both when
+    #  `$char` is a hyphen `-`, and when its a percent sign `%`
+    printf -- '%s' "$char"
+
+    # if we're in text mode, there's no more processing to do`
+    if [[ "$u_mode" == 'text' ]] continue
+
+    # —— Column Mode Processing ———————————————— #
     # add a left padding to the hex chars which need it
     if (( $#hex < u_zero_pad )) hex="${(l:$u_zero_pad::0:)hex}"
 
-    # always print the char itself
-    # note: this syntax seemed like the only thing that works with both when
-    #  $char is a hyphen, and when its a percent sign
-    printf -- '%s' "$char"
-    # and then if we're in column mode,
     #  print the hex code, separator, and a newline
     # also, make the hex code uppercase, and left-pad it with 5 spaces
-    if ! (( u_text_mode )) echo "  :  ${(Ul:5:: :)hex}"
-    # Note: when printing, make  ↑ sure a non-escapable char
+    if [[ "$u_mode" == 'list' ]] \
+      echo "  :  ${(Ul:5:: :)hex}"
+    # Note: when printing, make sure a non-escapable char
     #  comes after it, or it'll be mangled if $char is a backslash
   }
+
+  # —— Final Cleanup ——————————————————————————— #
   # final newline, since text mode is using `printf [-n]`
-  if (( u_text_mode )) echo
-  if (( u_debug     )) set +x
-  if (( u_verbose   )) see::line
+  if [[ "$u_mode" == 'text' ]] echo
+  if (( u_debug             )) set +x
+  if (( u_verbose           )) echo $_line
 }
 
 # ——————————————————————————————————————————————————————————————————————————— #
 
 # if the script's being run directly (i.e. not being sourced), then run tests
-#  equivalent to `if __name__ == "__main__"`
+# (this line is equivalent to `if __name__ == "__main__"`)
 if [[ $ZSH_EVAL_CONTEXT == 'toplevel' ]] {
-  # clear
+  local -r _line="${(r:$COLUMNS::─:)}"
+  clear; echo $_line
 
-  # echo -n "this is a normal•str" | see "$@"
-  see::line
-  echo -n $'this?→\x00, it\'s %%a\nlong•"str"-🖮\a␤ \\ 𱌮' | see "$@"
-  see::line
-  echo -n $'str w \x0 a\nnl' | see "$@"
-  see::line
-  echo -n $'\\\a\b\e\f\n\r\t\v' | see "$@"
-  see::line
-  echo $'test \e[31mstr\e[0m' | see "$@"
-  see::line
-  echo $'test ---- str\e[0m' | see "$@"
-  see::line
-  # cat $0 | see "$@"
+  echo -n "this is a normal•str"                  | see "$@"; echo "\n$_line"
+  echo -n $'this?→\x00, its %s\nlong•"str"-\a␤\\' | see "$@"; echo "\n$_line"
+  echo -n $'str w \x0 a\nnewline'                 | see "$@"; echo "\n$_line"
+  echo -n $'\\\a\b\e\f\n\r\t\v\''                 | see "$@"; echo "\n$_line"
+  echo $'test \e[31mstr\e[0m'                     | see "$@"; echo "\n$_line"
+  echo $'test ---- str\e[0m'                      | see "$@"; echo "\n$_line"
 
-  # cat ../resources/control_chars.txt | see "$@"
+  # cat ../resources/control_chars.txt              | see "$@"; echo $_line
+  # cat $0 | head -n 301 | tail -n $(( LINES - 3 )) | see "$@"; echo $_line
+  # cat $0                                          | see "$@"; echo $_line
+
+  # see::usage
 }
 
 # ——————————————————————————————————————————————————————————————————————————— #
 
 
-see::parse_opts() {
-  # local -rA _opts_AArr=( "$@" )
-  #
-  # echo "$_opts_AArr"
-  #
-  #
-  # # Note: a 'u_' prefix indicates a user-inputted value
-  # local -i 10 u_debug=0     # [bool] debug mode (implies verbose)
-  # local -i 10 u_verbose=0   # [bool] verbose mode
-  # local -i 10 u_text_mode=0 # [bool] show just text instead of columns
-  # local -i 10 u_width=32    # width for column mode (≈ xxd -c)
-  # local -i 10 u_zero_pad=2  # how many 0s to add before a hex code
-  # local u_esc_chars=''      # which set of esc chars to use. the options are:
-  # #                         #   unicode    c    caret    cdash    none
-  #   
-  # local -rA _options=(
-  #   [D]='0;1;-;*;0;early [D]ebug mode'
-  #   [d]='0;0;-;*;0;[d]ebug mode'
-  #   [v]='0;1;-;*;0;[v]erbose output'
-  #   [t]='0;1;-;C;0;[t]ext-only mode'
-  #   [C]='0;1;-;t;1;[C]olumn mode'
-  #   [w]='1;0;i;t;32;output [w]idth'
-  #   [0]='1;0;i;t;2;hex [0]-padding length'
-  #   [e]='1;0;l;t;unicode;[e]scape charset;unicode,c,caret,cdash,none'
-  #   [h]='0;0;-;-;-;[h]elp'
-  #   #0   1 2 3 4   5.&.6...............  7.....
-  #   # 0 - option character
-  #   # 1 - takes input?
-  #   # 2 - accepts `+`?
-  #   # 3 - arg type
-  #   #     - 'i'=int, 's'=str, 'l'=literal, '-'=n/a
-  #   # 4 - does not work with
-  #   #     - '*'=works w everything, '-'=works w nothing
-  #   # 5 - default value
-  #   #     - '-'=n/a
-  #   # 6 - descriptive name
-  #   # 7 - legal values (literals only)
-  #   #     - comma-delimited
-  # )
-  #
-  # while { getopts ':DdvtCw:0:e:' opt; } {
-  # 
-  #   echo "${(r:40::─:)}"
-  #   echo "opt  ==  '$opt'"
-  #   echo "arg  ==  '$OPTARG'"
-  # 
-  #   case "$opt" {
-  #   [Dd]) u_debug=1 u_verbose=1 ;;
-  #     v ) u_verbose=1           ;;
-  #     t ) u_text_mode=1         ;;
-  #     C ) u_text_mode=0         ;;
-  #     w ) u_width="$OPTARG"     ;;
-  #     0 ) u_zero_pad="$OPTARG"  ;;
-  #     e ) u_esc_chars="$OPTARG" ;;
-  #     h ) usage; return 0       ;;
-  #     * )
-  #       echo -n $'\e[31m'
-  #       if [[ "$opt" == '?' ]] { echo "$0: bad option: -$OPTARG" >&2; } \
-  #       else { echo "$0: -$OPTARG requires an argument" >&2; }  # $opt = ':'
-  #       echo -n $'\e[0m'
-  # 
-  #       continue
-  #       # usage
-  #       # return 1
-  #       ;;
-  #   }
-  #   if (( u_debug )) {
-  #     echo -n "\e[32m-$opt"
-  #     if [[ -n "$OPTARG" ]] echo -n " ${(qq)OPTARG}"
-  #     echo ' is a valid input\e[0m'
-  #   }
-  # }
-  #
-}
+# # These 3 can be generated rather than hardcoded - this is just temporary.
+# # Honestly I'll have to look into it, but the caret and unicode esc chars
+# #  might be able to be hardcoded as well
+# local -rA _cdash_esc_chars=(
+#   [esc_col]="$_caret_colour"
+#   [$'\u00']='\C-@' [$'\u01']='\C-A' [$'\u02']='\C-B' [$'\u03']='\C-C'
+#   [$'\u04']='\C-D' [$'\u05']='\C-E' [$'\u06']='\C-F' [$'\u07']='\C-G'
+#   [$'\u08']='\C-H' [$'\u09']='\C-I' [$'\u0A']='\C-J' [$'\u0B']='\C-K'
+#   [$'\u0C']='\C-L' [$'\u0D']='\C-M' [$'\u0E']='\C-N' [$'\u0F']='\C-O'
+#   [$'\u10']='\C-P' [$'\u11']='\C-Q' [$'\u12']='\C-R' [$'\u13']='\C-S'
+#   [$'\u14']='\C-T' [$'\u15']='\C-U' [$'\u16']='\C-V' [$'\u17']='\C-W'
+#   [$'\u18']='\C-X' [$'\u19']='\C-Y' [$'\u1A']='\C-Z' [$'\u1B']='\C-['
+#   [$'\u1C']='\C-\' [$'\u1D']='\C-]' [$'\u1E']='\C-^' [$'\u1F']='\C-_'
+#   [$'\u7F']='\C-?'
+# )
+# local -rA _hex_esc_chars=(
+#   [esc_col]="$_unicode_colour"
+#   [$'\u00']='0x00' [$'\u01']='0x01' [$'\u02']='0x02' [$'\u03']='0x02'
+#   [$'\u04']='0x04' [$'\u05']='0x05' [$'\u06']='0x06' [$'\u07']='0x06'
+#   [$'\u08']='0x08' [$'\u09']='0x09' [$'\u0A']='0x0A' [$'\u0B']='0x0A'
+#   [$'\u0C']='0x0C' [$'\u0D']='0x0D' [$'\u0E']='0x0E' [$'\u0F']='0x0E'
+#   [$'\u10']='0x10' [$'\u11']='0x11' [$'\u12']='0x12' [$'\u13']='0x12'
+#   [$'\u14']='0x14' [$'\u15']='0x15' [$'\u16']='0x16' [$'\u17']='0x16'
+#   [$'\u18']='0x18' [$'\u19']='0x19' [$'\u1A']='0x1A' [$'\u1B']='0x1A'
+#   [$'\u1C']='0x1C' [$'\u1D']='0x1D' [$'\u1E']='0x1E' [$'\u1F']='0x1E'
+#   [$'\u7F']='0x7F'
+# )
+# local -rA _uni_esc_esc_chars=(
+#   [esc_col]="$_unicode_colour"
+#   [$'\u00']=$'\\u00' [$'\u01']=$'\\u01' [$'\u02']=$'\\u02' [$'\u03']=$'\\u02'
+#   [$'\u04']=$'\\u04' [$'\u05']=$'\\u05' [$'\u06']=$'\\u06' [$'\u07']=$'\\u06'
+#   [$'\u08']=$'\\u08' [$'\u09']=$'\\u09' [$'\u0A']=$'\\u0A' [$'\u0B']=$'\\u0A'
+#   [$'\u0C']=$'\\u0C' [$'\u0D']=$'\\u0D' [$'\u0E']=$'\\u0E' [$'\u0F']=$'\\u0E'
+#   [$'\u10']=$'\\u10' [$'\u11']=$'\\u11' [$'\u12']=$'\\u12' [$'\u13']=$'\\u12'
+#   [$'\u14']=$'\\u14' [$'\u15']=$'\\u15' [$'\u16']=$'\\u16' [$'\u17']=$'\\u16'
+#   [$'\u18']=$'\\u18' [$'\u19']=$'\\u19' [$'\u1A']=$'\\u1A' [$'\u1B']=$'\\u1A'
+#   [$'\u1C']=$'\\u1C' [$'\u1D']=$'\\u1D' [$'\u1E']=$'\\u1E' [$'\u1F']=$'\\u1E'
+#   [$'\u7F']=$'\\u7F'
+# )
 
 
 # ——————————————————————————————————————————————————————————————————————————— #
 
-# spell:ignore cdash
+# spell:ignore cdash reprs
 
 # spell:ignoreRegexp /(?<=(^|\s)#.*\(.\)|\[.\])\w+/g
 # spell:ignoreRegexp /(?<=getopts) '[^']+'/g
+# spell:ignoreRegexp /(?<=\$\{b0\})\w+/g
 # spell:ignoreRegexp /\\(e|033|x1b)\[[0-9;]+?m\B/g
