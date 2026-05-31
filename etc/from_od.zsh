226daa518398d4005da30241726169ed50a72127
JV <jaiveer.chadda@gmail.com>
2026-06-01 02:31:33 +0400

    Cleaned up `from_od` slightly

--- etc/from_od.zsh
+++ etc/from_od.zsh
@@ -1,19 +1,17 @@
 #!/usr/bin/env zsh
 
-line_() { echo "${(r:$COLUMNS::─:)}"; }
-
 # Note: not really a viable function; just a proof of concept
-from_od fod () {
+function from_od fod () {
   local -r orig_text="${$( cat; echo '.' )%.}"
   local -r od_out="$( echo -nE "$orig_text" | od -a )"
 
   local -ra chars=( ${(@s: :)${(@f)od_out%$'\n'*}#* } )
 
-  local -r SPC=$'\e[34m·\e[0m'
-  local -r ESC=$'\e[38;5;231;48;5;088m␛\e[0m'
-  local -r NLN=$'\e[33m␤\n\e[0m'
-  local -r UNK=$'\e[38;5;231;48;5;088m'
-  local -r rst=$'\e[0m'
+  local -r SPC=$'\e[34m·\e[m'
+  local -r ESC=$'\e[38;5;231;48;5;88m␛\e[m'
+  local -r NLN=$'\e[33m␤\n\e[m'
+  local -r UNK=$'\e[38;5;231;48;5;88m'
+  local -r rst=$'\e[m'
 
   local -a formatted=( "${(@)chars}" )
   formatted=( "${(@)formatted:/sp/$SPC}"   )
@@ -24,10 +22,10 @@ from_od fod () {
   #  but not elems that start with `\e`, cos those are our formatted chars
   formatted=( "${(@*)formatted:/(#b)([^$'\e']?##)/$UNK$match[1]$rst}" )
 
-  # echo "$orig_text"        ; line_
-  # echo "$chars"            ; line_
-  # echo "$formatted"        ; line_
-  echo -En "${(j::)formatted}" # ; line_
+  # echo "$orig_text"        ; line
+  # echo "$chars"            ; line
+  # echo "$formatted"        ; line
+  echo -En "${(j::)formatted}" # ; line
 }
 
 if [[ $ZSH_EVAL_CONTEXT == 'toplevel' ]] {
