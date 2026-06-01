ccb99347f1be2e3df47331487b652af80056c655
JV <jaiveer.chadda@gmail.com>
2026-06-01 16:30:55 +0400

    Moved the input reading to its own file, so I can hopefully extend its functionality at some point

--- source/main.zsh
+++ source/main.zsh
@@ -4,6 +4,7 @@
   local -r source_dir="${${(%):-%x}:a:h}"
   source "$source_dir/usage.zsh"
   source "$source_dir/charsets.zsh"
+  source "$source_dir/read-input.zsh"
 }
 
 function see () {
@@ -136,19 +137,8 @@ function see () {
   # ————————————————————————————————————————————————————————————————————————— #
   # — Read Input Files —————————————————————————————————————————————————————— #
 
-  # fork out to `cat` to read the files
-  # `${u_file:-${@:--}}` is saying: first check if `$u_file` has some value.
-  #  if it doesn't, check the rest of the arguments `$@`.
-  #  if there's nothing in that, then read from stdin: `-`
-  local input; input="$(
-    cat "${u_file:-${@:--}}" || return $?
-    echo -n 'END'
-  )" || return $?  # if `cat` fails, exit immediately
-
-  # also, append an arbitrary string `END` to the end of the input, since the
-  #  `$(...)` construct removes all trailing newlines, so this allows us to
-  #  keep the trailing newline, after we remove the `END` string we added
-  input="${input%END}"
+  local input
+  see::read_input
 
   # ———————————————————————————————————————————————————————————————————————— #
   # — Pre-Processing ——————————————————————————————————————————————————————— #
