9d5b584cba4855d1722e1ec841a9d7d939c5debe
JV <jaiveer.chadda@gmail.com>
2026-06-29 00:48:09 +0530

    Reworked the input-reading functionality  to avoid using `cat`, and to only use zsh builtins
    The whole program is now 100% builtins - I don't fork out to any external utilities

--- source/read-input.zsh
+++ source/read-input.zsh
@@ -1,19 +1,28 @@
 #!/usr/bin/env zsh
 
+zmodload -F zsh/system b:sysread
+
 function see::read_input () {
+  local -a files_to_print=( "${(@)u_files}" "$@" )
+  if ! (( $#files_to_print )) files_to_print=( - )
 
-  # fork out to `cat` to read the files
-  # `${u_file:-${@:--}}` is saying:
-  #  - first check if `$u_file` has some value.
-  #  - then check the arguments `$@`.
-  #  - finally, if there's nothing in any of those, then read from stdin: `-`
-  input="$(
-    cat "${(@)u_files:-${@:--}}" || return $?
-    echo -n 'END'
-  )" || return $?  # if `cat` fails, exit immediately
+  local -ri 10 chunk_size=$(( 1 << 13 ))  # 8 kb
+  local  -i 10 fd
+  local  -i 2  from_stdin
 
-  # also, append an arbitrary string `END` to the end of the input, since the
-  #  `$(...)` construct removes all trailing newlines, so this allows us to
-  #  keep the trailing newline, after we remove the `END` string we added
-  input="${input%END}"
+  local file temp_input
+  input=
+
+  for file in "${(@)files_to_print}"; {
+    from_stdin=0
+
+    if [[ "$file" == (-|/dev/stdin|/(proc/self|dev)/fd/0) ]] from_stdin=1
+    if (( from_stdin )) { fd=0; } else { exec {fd}<"$file"; }
+
+    # read from `$fd` chunk by chunk
+    while { sysread -i $fd -s $chunk_size temp_input; } input+="$temp_input"
+
+    # close `$fd`
+    if ! (( from_stdin )) exec {fd}<&-
+  }
 }
