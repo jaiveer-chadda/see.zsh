a77dfb998ead67292eb3dff4570bb0bcef0939d0
JV <jaiveer.chadda@gmail.com>
2026-06-29 01:21:26 +0530

    Simplified and commented the new `read_input` function

--- source/read-input.zsh
+++ source/read-input.zsh
@@ -1,28 +1,34 @@
 #!/usr/bin/env zsh
 
+# load the `sysread` builtin - this is a frontend to the `read` syscall
+#  we're using `sysread` instead of `read`, since `read` can't handle nullbytes
 zmodload -F zsh/system b:sysread
 
 function see::read_input () {
+  # figure out whether the inputted filename actually points to stdin
+  local -r _is_stdin='(-|/dev/stdin|/(proc/self|dev)/fd/0)'
+
+  # combine the files passed via the `-f` option with
+  #  anything left in the positional args
   local -a files_to_print=( "${(@)u_files}" "$@" )
+
+  # if nothing was inputted, then read from stdin `-`
   if ! (( $#files_to_print )) files_to_print=( - )
 
-  local -ri 10 chunk_size=$(( 1 << 13 ))  # 8 kb
-  local  -i 10 fd
-  local  -i 2  from_stdin
-
-  local file temp_input
-  input=
+  local -i 10 fd
+  local file chunk
 
   for file in "${(@)files_to_print}"; {
-    from_stdin=0
+    fd=-1  # an arbitrary, non-zero number
 
-    if [[ "$file" == (-|/dev/stdin|/(proc/self|dev)/fd/0) ]] from_stdin=1
-    if (( from_stdin )) { fd=0; } else { exec {fd}<"$file"; }
+    # if we aren't reading from stdin, then open the file for reading as a
+    #  new file descriptor, and store its number in `$fd`
+    if [[ "$file" == ${~_is_stdin} ]] { fd=0; } else { exec {fd}<"$file"; }
 
-    # read from `$fd` chunk by chunk
-    while { sysread -i $fd -s $chunk_size temp_input; } input+="$temp_input"
+    # read from `$fd` in 8kb chunks
+    while { sysread -i $fd chunk; } input+="$chunk"
 
-    # close `$fd`
-    if ! (( from_stdin )) exec {fd}<&-
+    # if we opened a custom fd, then, now that we're done, close `$fd`
+    if (( fd != 0 )) exec {fd}<&-
   }
 }
